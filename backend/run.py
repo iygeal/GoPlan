62f13b839c185f6aae1cc6b4f38e2e772bcd3c6c
VictorNalu <victorchinalurumogu@gmail.com>
2024-10-26 04:55:51 +0100

    login/signup/homepage_updated

--- backend/run.py
+++ backend/run.py
@@ -4,9 +4,13 @@ This module runs the GoPlan Flask app.
 """
 
 from app import create_app
+from flask_cors import CORS
 
 # Create an instance of the app
 app = create_app()
+
+# Enable CORS
+CORS(app)
 
 if __name__ == '__main__':
     # Run the Flask app
