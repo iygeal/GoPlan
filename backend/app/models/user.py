4903e483fb88b4b580f541ab81fc1cdf761923a4
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-11-01 23:31:13 +0100

    added new methods to the user models

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -24,3 +24,27 @@ class User(BaseModel):
     # Relationships
     dashboard = db.relationship('Dashboard', back_populates='user',
                                  uselist=False)
+
+    def validate_email(self):
+        """Validates the email format using a regex."""
+        email_regex = r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if not re.match(email_regex, self.email):
+            raise ValueError(f"Invalid email format: {self.email}")
+
+    def set_password(self, password):
+        """Hash and set the user's password."""
+        self.password = bcrypt.hashpw(password.encode(
+            'utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, password):
+        """Check if the provided password matches the hashed password."""
+        return bcrypt.checkpw(
+            password.encode('utf-8'), self.password.encode('utf-8'))
+
+    def to_dict(self):
+        """Convert object to dictionary and
+        exclude sensitive fields like password.
+        """
+        user_dict = super().to_dict()
+        user_dict.pop('password', None)  # Exclude password
+        return user_dict
